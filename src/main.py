f6a93f8fb6fa7f3782836396b5c212bf029ca8de
Laila Ainur Rahma <111741203+laainra@users.noreply.github.com>
2023-03-24 18:13:57 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+# Mengimport seluruh module yangada pada direktori luas dan volume
 from luas.lingkaran import *
 from luas.jajargenjang import *
 from luas.persegi import *
@@ -13,60 +14,60 @@ from volume.limas import *
 from volume.prisma import *
 
 
-
+#mendeklarasikan function main() atau fungsi utama
 def main():
-    choice = int(input("Pilihan Bangun\n1. 2 Dimensi\n2. 3 Dimensi\nMasukkan bangun:"))
-    if choice == 1:
-        print("Pilihan Bangun\n1. Persegi\n2. Persegi Panjang\n3. Segitiga\n4. Lingkaran\n5. Jajar Genjang\n6. Trapesium")
-        twoD = int(input("Masukkan bangun 2D 1 - 6:"))
-        if twoD == 1:
-            luasPersegi()
-        elif twoD == 2:
-            luasPersegiPanjang()
-        elif twoD == 3:
-            luasSegitiga()
-        elif twoD == 4:
-            luasLingkaran()
-        elif twoD == 5:
-            luasJajargenjang()
-        elif twoD == 6:
-            luasTrapesium()
+    choice = int(input("Pilihan Bangun\n1. 2 Dimensi\n2. 3 Dimensi\nMasukkan bangun:")) # meminta input pilihan dimensi bangun
+    if choice == 1: # jika pilihan bangun 2D
+        print("Pilihan Bangun\n1. Persegi\n2. Persegi Panjang\n3. Segitiga\n4. Lingkaran\n5. Jajar Genjang\n6. Trapesium") # menampilkan pilihan bangun 2D
+        twoD = int(input("Masukkan bangun 2D 1 - 6:")) # meminta input pilihan bangun 2D
+        if twoD == 1:  # jika memilih bangun persegi
+            luasPersegi()  #memanggil function perhitungan bangun
+        elif twoD == 2: # jika memilih bangun persegi panjang
+            luasPersegiPanjang() #memanggil function perhitungan bangun
+        elif twoD == 3: # jika memilih bangun segitiga
+            luasSegitiga() #memanggil function perhitungan bangun
+        elif twoD == 4: # jika memilih bangun lingkaran
+            luasLingkaran() #memanggil function perhitungan bangun
+        elif twoD == 5: # jika memilih bangun jajar genjang
+            luasJajargenjang() #memanggil function perhitungan bangun
+        elif twoD == 6: # jika memilih bangun trapesium
+            luasTrapesium() #memanggil function perhitungan bangun
         else:
-            print("OOPSS, Pilihan kamu invalid. ")
-            pil = input("Coba lagi? y/n").lower
+            print("OOPSS, Pilihan kamu invalid. ") # jika memilih pilihan yang tidak valid
+            pil = input("Coba lagi? y/n").lower # input untuk mengulang atau tidak
             if pil=="y":
-                main()
+                main()# jika memilih untuk mengulang, jalankan fungsi main()
             else:
-                exit()
-    elif choice == 2:
-        print("Pilihan Bangun\n1. Persegi\n2. Persegi Panjang\n3. Segitiga\n4. Lingkaran\n5. Jajar Genjang\n6. Trapesium")
-        twoD = int(input("Masukkan bangun 3D 1 - 6:"))
-        if twoD == 1:
-            volumeKubus()
-        elif twoD == 2:
-            volumeBalok()
-        elif twoD == 3:
-            volumeTabung()
-        elif twoD == 4:
-            volumeKerucut()
-        elif twoD == 5:
-            volumeLimas()
-        elif twoD == 6:
-            volumePrisma()
+                exit() # jika tidak, keluar dari program
+    elif choice == 2: # jika memilih bangun 3D
+        print("Pilihan Bangun\n1. Persegi\n2. Persegi Panjang\n3. Segitiga\n4. Lingkaran\n5. Jajar Genjang\n6. Trapesium") # menampilkan pilihan bangun 3D
+        twoD = int(input("Masukkan bangun 3D 1 - 6:")) # meminta input pilihan bangun 3D
+        if twoD == 1: # jika memilih bangun kubus
+            volumeKubus() #memanggil function perhitungan bangun
+        elif twoD == 2: # jika memilih bangun balok
+            volumeBalok() #memanggil function perhitungan bangun
+        elif twoD == 3: # jika memilih bangun tabung
+            volumeTabung() #memanggil function perhitungan bangun
+        elif twoD == 4: # jika memilih bangun kerucut
+            volumeKerucut() #memanggil function perhitungan bangun
+        elif twoD == 5: # jika memilih bangun limas
+            volumeLimas() #memanggil function perhitungan bangun
+        elif twoD == 6: # jika memilih bangun prisma
+            volumePrisma() #memanggil function perhitungan bangun
         else:
-            print("OOPSS, Pilihan kamu invalid. ")
-            pil = input("Coba lagi? y/n").lower
+            print("OOPSS, Pilihan kamu invalid. ") # jika memilih pilihan yang tidak valid
+            pil = input("Coba lagi? y/n").lower # input untuk mengulang atau tidak
             if pil=="y":
-                main()
+                main() # jika memilih untuk mengulang, jalankan fungsi main()
             else:
-                exit()
+                exit() # jika tidak, keluar dari program
     else:
-        print("OOPSS, Pilihan kamu invalid. ")
-        pil = input("Coba lagi? y/n").lower
+        print("OOPSS, Pilihan kamu invalid. ") # jika memilih pilihan yang tidak valid
+        pil = input("Coba lagi? y/n").lower # input untuk mengulang atau tidak
         if pil=="y":
-            main()
+            main() # jika memilih untuk mengulang, jalankan fungsi main()
         else:
-            exit()
+            exit() # jika tidak, keluar dari program
 
-main()
+main() # menjalankan function main()
 
